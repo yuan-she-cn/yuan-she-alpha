aa3ada2c9a51508cea67f2472d97cace5cdc134e
wuxingping <13087846662@163.com>
2024-07-04 17:40:14 +0800

    格式化文件

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import LayoutContent from "../views/LayoutContent.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +6,7 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: LayoutContent,
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/LayoutContent.vue"),
+      component: () => import("@/views/LayoutContent.vue"),
     },
   ],
 });
